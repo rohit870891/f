e137d7f2ef6c34ede2ed831f708161e7b8452a1d
rohit870891 <koyeb77user@gmail.com>
2025-05-01 14:35:05 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -211,8 +211,8 @@ async def start_command(client: Client, message: Message):
                 mention=message.from_user.mention,
                 id=message.from_user.id
             ),
-            reply_markup=reply_markup,
-            message_effect_id=5104841245755180586)  # 🔥
+            reply_markup=reply_markup)#,
+            #message_effect_id=5104841245755180586#)  # 🔥
         
         return
 
